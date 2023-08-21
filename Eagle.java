e9aae01067bbf1347b2d9261f9760f759905ed1c
Tommy Adam <tommyadam1992@gmail.com>
2023-08-21 10:26:50 +0200

    Creating 'fly' Interface, implementing it into Eagle, uncommenting in Nature

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,33 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(getName() + " takes off in the sky.");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        altitude += meters;
+        System.out.println(getName() + " flies upward, altitude: " + altitude);
+        return altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        altitude -= meters;
+        System.out.println(getName() + " flies downward, altitude: " + altitude);
+        return altitude;
+    }
+
+    @Override
+    public void land() {
+        if (altitude > 1) {
+            System.out.println(getName() + " is too high, it can't land.");
+        } else {
+            System.out.println(getName() + " lands on the ground.");
+        }
     }
 }
