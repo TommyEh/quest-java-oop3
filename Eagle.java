b939e7ad5be4cda8a7894486ce484abd553f4a7f
Tommy Adam <tommyadam1992@gmail.com>
2023-08-21 10:34:33 +0200

    Updating Eagle.java

--- Eagle.java
+++ Eagle.java
@@ -24,20 +24,27 @@ public class Eagle extends Bird implements Fly {
 
     @Override
     public void takeOff() {
-        System.out.println(getName() + " takes off in the sky.");
+        if (!flying) {
+            flying = true;
+            System.out.println(getName() + " takes off in the sky.");
+        }
     }
 
     @Override
     public int ascend(int meters) {
-        altitude += meters;
-        System.out.println(getName() + " flies upward, altitude: " + altitude);
+        if (flying) {
+            altitude += meters;
+            System.out.println(getName() + " flies upward, altitude: " + altitude);
+        }
         return altitude;
     }
 
     @Override
     public int descend(int meters) {
-        altitude -= meters;
-        System.out.println(getName() + " flies downward, altitude: " + altitude);
+        if (flying) {
+            altitude = Math.max(altitude - meters, 0);
+            System.out.println(getName() + " flies downward, altitude: " + altitude);
+        }
         return altitude;
     }
 
@@ -46,6 +53,7 @@ public class Eagle extends Bird implements Fly {
         if (altitude > 1) {
             System.out.println(getName() + " is too high, it can't land.");
         } else {
+            flying = false;
             System.out.println(getName() + " lands on the ground.");
         }
     }
